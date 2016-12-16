79c7711a638dfd1dbc04e865a48ba5fa9da898b3
pingwindyktator <ja2222ja@gmail.com>
2016-12-16 21:15:17 +0100

    stalker.stalk implemented

--- plugins/stalker.py
+++ plugins/stalker.py
@@ -46,7 +46,6 @@ class stalker(plugin):
         if not args: return
         nick = args[0]
         result = [host for host in self.database if nick in self.database[host]]
-        response = ''
         if result:
             response = 'known hosts of %s: %s ' % (nick, result)
         else:
@@ -55,14 +54,28 @@ class stalker(plugin):
         self.logger.info('%s asks about hosts of %s: %s' % (sender_nick, nick, result))
 
     @command
+    def stalk(self, sender_nick, args):
+        if not args: return
+        nick = args[0]
+        result = []
+        for x in self.database.values():
+            if nick in x: result.extend(x)
+
+        if result:
+            response = 'other nicks of %s: %s' % (nick, result)
+        else:
+            response = "I know nothing about %s" % nick
+        self.bot.send_response_to_channel(response)
+        self.logger.info('%s stalks %s: %s' % (sender_nick, nick, result))
+
+    @command
     def stalk_host(self, sender_nick, args):
         if not args: return
-        host = args[0]
-        result = self.database[host] if host in self.database else None
-        response = ''
-        if result:
-            response = 'known nicks from %s: %s' % (host, result)
+        nick = args[0]
+        hosts = self.database[nick] if nick in self.database else None
+        if hosts:
+            response = 'known nicks from %s: %s' % (nick, hosts)
         else:
-            response = "I know nothing about %s" % host
+            response = "I know nothing about %s" % nick
         self.bot.send_response_to_channel(response)
-        self.logger.info('%s asks about nicks from %s: %s' % (sender_nick, host, result))
+        self.logger.info('%s asks about nicks from %s: %s' % (sender_nick, nick, hosts))
