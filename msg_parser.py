a5500fb2fe69479fb232c0cf45359f93b7999dcb
pingwindyktator <ja2222ja@gmail.com>
2017-04-08 23:27:49 +0200

    'except: pass' WTF #2

--- msg_parser.py
+++ msg_parser.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 
 def split_msg(msg):
@@ -20,11 +21,13 @@ def trim_msg(to_trim, full_msg):
     """
     splited_msg = split_msg(full_msg)
     command = ''
+    logger = logging.getLogger(__name__)
 
     try:
         if len(splited_msg) > 0 and full_msg.startswith(to_trim):
             to_trim_impl = re.compile(to_trim + r'\W*').findall(full_msg)[0]
             command = full_msg.replace(to_trim_impl, '', 1)
-    except: pass
+    except Exception as e:
+        logger.error('exception caught while parsing msg: ' + e)
 
     return command
