a833c4ad2349996dd4ac383b81344abcdfa77395
pingwindyktator <ja2222ja@gmail.com>
2017-04-12 01:15:46 +0200

    echo command refactored

--- plugins/echo.py
+++ plugins/echo.py
@@ -7,13 +7,8 @@ class echo(plugin):
         super().__init__(bot)
         self.logger = logging.getLogger(__name__)
 
-    def on_pubmsg(self, connection, raw_msg):
-        full_msg = raw_msg.arguments[0]
-        command = msg_parser.trim_msg(self.bot.get_command_prefix(), full_msg)
-        commands = msg_parser.split_msg(command)
-
-        if len(commands) > 0 and commands[0] == 'echo':
-            to_trim = re.compile('echo *').findall(command)[0]
-            response = command.replace(to_trim, '', 1)
-            self.bot.send_response_to_channel(response)
-            self.logger.info("echo '%s' for %s" % (response, raw_msg.source.nick))
+    @command
+    @do_not_parse_args
+    def echo(self, sender_nick, msg):
+        self.bot.send_response_to_channel(msg)
+        self.logger.info("echo '%s' for %s" % (msg, msg))
