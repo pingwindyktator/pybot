9b0c7362e57f3ec69e31f4ce1b9ebf1cf1dcdf44
pingwindyktator <ja2222ja@gmail.com>
2017-04-23 20:45:27 +0200

    webtitle_parser is much more smarter now

--- plugins/webtitle_parser.py
+++ plugins/webtitle_parser.py
@@ -11,24 +11,25 @@ class webtitle_parser(plugin):
         self.logger = logging.getLogger(__name__)
 
     def on_pubmsg(self, msg, **kwargs):
-        url = msg.strip().split()[0]
+        urls = msg.strip().split()
+        regex = re.compile(
+            r'^(?:http)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
+        for url in urls: self.parse_url(url, regex)
+
+    def parse_url(self, url, regex):
         try:
-            regex = re.compile(
-                r'^(?:http|ftp)s?://'  # http:// or https://
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-                r'localhost|'  # localhost...
-                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-                r'(?::\d+)?'  # optional port
-                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
             if regex.findall(url):
                 req = requests.get(url, timeout=5)
                 tree = fromstring(req.content)
                 title = tree.findtext('.//title')
                 if title is not None and title != '':
                     self.bot.send_response_to_channel(color.light_green(title))
-        except (requests.HTTPError, requests.ConnectionError, requests.RequestException):
+
+        except Exception:
             self.logger.info('possibly invalid URL: %s', url)
-        except Exception as e:
-            self.logger.error('exception caught: %s', e)
