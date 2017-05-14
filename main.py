b8b3360e09bde9043e77dd3e064c5b46ae942e98
pingwindyktator <ja2222ja@gmail.com>
2017-05-15 01:32:11 +0200

    main.py refactored

--- main.py
+++ main.py
@@ -7,24 +7,20 @@ def main():
         print("Python 3.6.0 required to run pybot")
         sys.exit(4)
 
+    import _main
+    _main.main()
+
+
+if __name__ == "__main__":
     try:
-        import _main
+        main()
     except (ImportError, ModuleNotFoundError) as e:
         name = e.name
         path = f' located at {e.path}' if e.path else ''
         print(f"No module named {name}{path}. Please try 'pip install -r ./requirements.py'")
-        return
-
-    try:
-        _main.main()
     except Exception as e:
         print(f'Internal error occurred: {e}. Please contact  ja222ja@gmail.com  with  pybot.error  file (paths to files will be compromised).')
         with open('pybot.error', 'a') as file:
             file.write('**********************************************************************************************\n')
             file.write(traceback.format_exc())
             file.write('**********************************************************************************************\n')
-
-        sys.exit(5)
-
-if __name__ == "__main__":
-    main()
