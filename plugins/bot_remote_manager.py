e23c947f350376d9166bc216402b67733745c618
pingwindyktator <ja2222ja@gmail.com>
2017-05-21 23:12:42 +0200

    bot_remote_manager: docs added

--- plugins/bot_remote_manager.py
+++ plugins/bot_remote_manager.py
@@ -7,18 +7,21 @@ class bot_remote_manager(plugin):
 
     @command
     @admin
+    @doc('disconnect from server')
     def disconnect(self, sender_nick, **kwargs):
         self.logger.warning(f'disconnect by {sender_nick}')
         self.bot.disconnect(self.config['disconnect_msg'])
 
     @command
     @admin
+    @doc('kill pybot')
     def die(self, sender_nick, **kwargs):
         self.logger.warning(f'die by {sender_nick}')
         self.bot.die('[die]')
 
     @command
     @admin
+    @doc('cycle the channel')
     def cycle(self, sender_nick, **kwargs):
         self.logger.warning(f'cycle by {sender_nick}')
         self.bot.leave_channel()
@@ -26,6 +29,7 @@ class bot_remote_manager(plugin):
 
     @command
     @admin
+    @doc('reconnect to server')
     def reconnect(self, sender_nick, **kwargs):
         self.logger.warning(f'reconnect by {sender_nick}')
         self.bot.connection.reconnect()
