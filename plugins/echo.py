e997436242674e9bb679512520991e11cbfc15d9
pingwindyktator <ja2222ja@gmail.com>
2017-05-22 00:16:03 +0200

    echo: docs added

--- plugins/echo.py
+++ plugins/echo.py
@@ -6,6 +6,7 @@ class echo(plugin):
         super().__init__(bot)
 
     @command
+    @doc('echo <message>: force bot to say <message>')
     def echo(self, sender_nick, msg, **kwargs):
         self.bot.say(msg)
         self.logger.info(f"echo '{msg}' for {sender_nick}")
