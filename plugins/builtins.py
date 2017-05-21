3061368a7c550d25face3c5a71d1cc4ccf78e56d
pingwindyktator <ja2222ja@gmail.com>
2017-05-21 22:22:53 +0200

    self_update: msgs improved

--- plugins/builtins.py
+++ plugins/builtins.py
@@ -221,12 +221,12 @@ class builtins(plugin):
         origin.fetch()
         origin.pull()
         if repo.head.orig_head().commit == repo.head.commit:
-            self.logger.info(f'already up to date at {repo.head.commit}')
-            self.bot.say(f'already up to date at "{str(repo.head.commit)[:6]}: {repo.head.commit.message.strip()}"')
+            self.logger.info(f'already up-to-date at {repo.head.commit}')
+            self.bot.say(f'already up-to-date at "{str(repo.head.commit)[:6]}: {repo.head.commit.message.strip()}"')
             return
 
         self.logger.warning(f'updated {repo.head.orig_head().commit} -> {repo.head.commit}')
-        diff_str = f', diffs at {", ".join([x.a_path for x in repo.head.commit.diff(repo.head.orig_head().commit)])}'
+        diff_str = f', diffs in {", ".join([x.a_path for x in repo.head.commit.diff(repo.head.orig_head().commit)])}'
 
         try:
             if self.update_config(): config_updated_str = ', config file updated'
