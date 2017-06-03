b81383dbd3f3ed23d13b733c692b17c814889db8
pingwindyktator <ja2222ja@gmail.com>
2017-06-03 03:28:21 +0200

    ensure_config_is_ok refactored #2

--- utils.py
+++ utils.py
@@ -61,7 +61,7 @@ def ensure_config_is_ok(config):
         'ignored_users': config_key_info(False, list),
         'file_logging_level': config_key_info(True, str),
         'stdout_logging_level': config_key_info(True, str),
-        'command_prefix': config_key_info(True, str),
+        'command_prefix': config_key_info(True, str)
     }
 
     for key, value in config.items():
@@ -77,12 +77,14 @@ def ensure_config_is_ok(config):
             c_assert(type(config[key]) is key_info.type, f'{key} field type should be {key_info.type.__name__}')
 
     c_assert(config['server'].strip(), 'you have to specify server address')
-
     c_assert(config['port'] >= 1024, 'port should be >= 1024')
     c_assert(config['port'] <= 49151, 'port should be <= 49151')
     c_assert(config['channel'].startswith('#'), 'channel should start with #')
     c_assert(config['nickname'], 'you have to specify at least one nickname to use')
     c_assert(config['max_autorejoin_attempts'] >= 0, 'max_autorejoin_attempts should be >= 0')
+    c_assert(config['file_logging_level'] in logging_level_str_to_int, f'file_logging_level can be one of: {", ".join((logging_level_str_to_int.keys()))}')
+    c_assert(config['stdout_logging_level'] in logging_level_str_to_int, f'stdout_logging_level can be one of: {", ".join((logging_level_str_to_int.keys()))}')
+    c_assert(config['command_prefix'].strip(), 'you have to specify command prefix')
 
     if 'disabled_plugins' in config:
         c_assert('enabled_plugins' not in config, 'you cannot have both enabled_plugins and disabled_plugins specified')
@@ -90,9 +92,8 @@ def ensure_config_is_ok(config):
     if 'enabled_plugins' in config:
         c_assert('disabled_plugins' not in config, 'you cannot have both enabled_plugins and disabled_plugins specified')
 
-    c_assert(config['file_logging_level'] in logging_level_str_to_int, f'file_logging_level can be one of: {", ".join((logging_level_str_to_int.keys()))}')
-    c_assert(config['stdout_logging_level'] in logging_level_str_to_int, f'stdout_logging_level can be one of: {", ".join((logging_level_str_to_int.keys()))}')
-    c_assert(config['command_prefix'].strip(), 'you have to specify command prefix')
+    for nickname in config['nickname']:
+        c_assert(nickname.strip(), 'you cannot have empty nickname')
 
 
 class yaml_config(dict):
