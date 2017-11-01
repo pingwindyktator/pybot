10bf520218710e4bab8b194765ef0c03ff941961
pingwindyktator <ja2222ja@gmail.com>
2017-11-01 11:23:06 +0100

    main.py: dumb typos fixed

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ def main():
 
 if __name__ == "__main__":
     if sys.version_info < (3, 6, 0):
-        print("Python 3.6.0 required to run pybot")
+        print('Python 3.6.0 required to run pybot')
         sys.exit(4)
 
     try:
         main()
-        open('pybot.error', 'w').close()
     except (ImportError, ModuleNotFoundError) as e:
         name = e.name
         path = ' located at %s' % e.path if e.path else ''
         print("No module named %s%s. Please try 'pip install -r requirements.txt'" % (name, path))
     except Exception as e:
-        print('Internal error occurred: %s. Please contact  ja222ja@gmail.com  with  pybot.error  file (file paths will be compromised).' % e)
+        print('Internal error occurred: %s. Please contact  ja2222ja@gmail.com  with  pybot.error  file (file paths will be compromised).' % e)
         open('pybot.error', 'w').close()
         with open('pybot.error', 'a') as error_file:
             with open('pybot.log') as log_file:
