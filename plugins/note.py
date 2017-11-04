eaf2623398c7d7832d1ed6703d3d1991b16fabe6
pingwindyktator <ja2222ja@gmail.com>
2017-11-04 01:43:21 +0100

    note.anote command implemented

--- plugins/note.py
+++ plugins/note.py
@@ -33,29 +33,25 @@ class note(plugin):
     @command
     @doc('note <nickname> <message>: store <message> for <nickname>')
     def note(self, sender_nick, msg, **kwargs):
+        self.note_impl(sender_nick, msg, anon=False)
+
+    @command
+    @doc('anote <nickname> <message>: store anonymous <message> for <nickname>')
+    def anote(self, sender_nick, msg, **kwargs):
+        self.note_impl(sender_nick, msg, anon=True)
+
+    def note_impl(self, sender_nick, msg, anon):
         if not msg: return
         target = msg.split()[0]
-        if target == self.bot.get_nickname():
-            self.bot.say("you won't trick me")
+        if sender_nick == target or sender_nick == self.bot.get_nickname():
+            self.bot.say('orly?')
             return
-        
+
         new_note = msg[len(target):].strip()
-        self.logger.info(f'{sender_nick} notes "{new_note}" for {target}')
         if not new_note: return
-        new_note = f'{datetime.now().strftime("%d-%m-%Y %H:%M")}  <{sender_nick}> {new_note}'
-
-        saved_notes = self.get_notes_for_user(target)
-
-        if saved_notes:
-            saved_notes.extend([new_note])
-            with self.db_mutex:
-                self.db_cursor.execute(f"UPDATE '{self.db_name}' SET notes = ? WHERE nickname = ?", (json.dumps(saved_notes), target.casefold()))
-                self.db_connection.commit()
-        else:
-            with self.db_mutex:
-                self.db_cursor.execute(f"INSERT INTO '{self.db_name}' VALUES (?, ?)", (target.casefold(), json.dumps([new_note])))
-                self.db_connection.commit()
-
+        self.logger.info(f'{sender_nick} notes "{new_note}" for {target}')
+        new_note = self.build_msg(None if anon else sender_nick, new_note)
+        self.save_note(target, new_note)
         self.bot.say_ok()
 
     def get_notes_for_user(self, nickname):
@@ -67,3 +63,22 @@ class note(plugin):
             result = list(json.loads(result[0]))
 
         return result
+
+    def build_msg(self, nickname, msg):
+        time = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+        if nickname: return f'{time}  <{nickname}> {msg}'
+        return f'{time}  {msg}'
+
+    def save_note(self, target, new_note):
+        saved_notes = self.get_notes_for_user(target)
+
+        if saved_notes:
+            saved_notes.extend([new_note])
+            with self.db_mutex:
+                self.db_cursor.execute(f"UPDATE '{self.db_name}' SET notes = ? WHERE nickname = ?", (json.dumps(saved_notes), target.casefold()))
+                self.db_connection.commit()
+        else:
+            with self.db_mutex:
+                self.db_cursor.execute(f"INSERT INTO '{self.db_name}' VALUES (?, ?)", (target.casefold(), json.dumps([new_note])))
+                self.db_connection.commit()
