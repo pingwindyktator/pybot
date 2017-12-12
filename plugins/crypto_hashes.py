5d1f91a0e8b336749d527ffc75bdbd6f274ff99b
pingwindyktator <ja2222ja@gmail.com>
2017-12-13 00:48:07 +0100

    crypto_hashes: more hashes

--- plugins/crypto_hashes.py
+++ plugins/crypto_hashes.py
@@ -6,6 +6,14 @@ from plugin import *
 
 class crypto_hashes(plugin):
     def __init__(self, bot):
+        if 'dsaWithSHA' not in hashlib.algorithms_available: del crypto_hashes.dsaWithSHA
+        if 'blake2s' not in hashlib.algorithms_available: del crypto_hashes.blake2s
+        if 'md4' not in hashlib.algorithms_available: del crypto_hashes.md4
+        if 'ecdsa-with-SHA1' not in hashlib.algorithms_available: del crypto_hashes.ecdsa_with_SHA1
+        if 'dsaEncryption' not in hashlib.algorithms_available: del crypto_hashes.dsaEncryption
+        if 'whirlpool' not in hashlib.algorithms_available: del crypto_hashes.whirlpool
+        if 'ripemd160' not in hashlib.algorithms_available: del crypto_hashes.ripemd160
+
         super().__init__(bot)
 
     @doc('computes sha1 of utf-8 encoded msg')
@@ -72,8 +80,57 @@ class crypto_hashes(plugin):
     @command
     def crc32(self, msg, **kwargs):
         self.bot.say(zlib.crc32(msg.encode()))
-        
+
     @doc('computes adler32 of utf-8 encoded msg')
     @command
     def adler32(self, msg, **kwargs):
         self.bot.say(zlib.adler32(msg.encode()))
+
+    @doc('computes ripemd160 of utf-8 encoded msg')
+    @command
+    def ripemd160(self, msg, **kwargs):
+        h = hashlib.new('ripemd160')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes whirlpool of utf-8 encoded msg')
+    @command
+    def whirlpool(self, msg, **kwargs):
+        h = hashlib.new('whirlpool')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes dsaEncryption of utf-8 encoded msg')
+    @command
+    def dsaEncryption(self, msg, **kwargs):
+        h = hashlib.new('dsaEncryption')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes ecdsa-with-SHA1 of utf-8 encoded msg')
+    @command
+    def ecdsa_with_SHA1(self, msg, **kwargs):
+        h = hashlib.new('ecdsa-with-SHA1')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes md4 of utf-8 encoded msg')
+    @command
+    def md4(self, msg, **kwargs):
+        h = hashlib.new('md4')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes blake2s of utf-8 encoded msg')
+    @command
+    def blake2s(self, msg, **kwargs):
+        h = hashlib.new('blake2s')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
+
+    @doc('computes dsaWithSHA of utf-8 encoded msg')
+    @command
+    def dsaWithSHA(self, msg, **kwargs):
+        h = hashlib.new('dsaWithSHA')
+        h.update(msg.encode())
+        self.bot.say(h.hexdigest())
