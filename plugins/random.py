8355cee7948080e920cb1546054176b9e5f6a629
pingwindyktator <pingwindyktator@users.noreply.github.com>
2018-02-09 11:12:18 +0100

    Update random.py

--- plugins/random.py
+++ plugins/random.py
@@ -56,38 +56,38 @@ class random(plugin):
 
         self.bot.say(result)
 
-    @doc("""random <min> <max>: fetch random number in [min, max) from random.org
-            random: fetch random number in [0, 1000000000) from random.org""")
+    @doc("""random <min> <max>: fetch random number in [min, max] from random.org
+            random: fetch random number in [0, 1000000000] from random.org""")
     @command
     def random(self, **kwargs):
         return self.random10(**kwargs)
 
-    @doc("""random10 <min> <max>: fetch random number in [min, max) from random.org
-            random10: fetch random number in [0, 1000000000) from random.org""")
+    @doc("""random10 <min> <max>: fetch random number in [min, max] from random.org
+            random10: fetch random number in [0, 1000000000] from random.org""")
     @command
     def random10(self, args, sender_nick, **kwargs):
         result, error = self.random_impl(args, 10, sender_nick)
         if result: self.bot.say(result)
         else: self.bot.say(error)
 
-    @doc("""random2 <min> <max>: fetch binary random number in [min, max) from random.org
-            random2: fetch binary random number in [0, 1000000000) from random.org""")
+    @doc("""random2 <min> <max>: fetch binary random number in [min, max] from random.org
+            random2: fetch binary random number in [0, 1000000000] from random.org""")
     @command
     def random2(self, args, sender_nick,  **kwargs):
         result, error = self.random_impl(args, 2, sender_nick)
         if result: self.bot.say(f'0b{result}')
         else: self.bot.say(error)
 
-    @doc("""random8 <min> <max>: fetch oct random number in [min, max) from random.org
-            random8: fetch oct random number in [0, 1000000000) from random.org""")
+    @doc("""random8 <min> <max>: fetch oct random number in [min, max] from random.org
+            random8: fetch oct random number in [0, 1000000000] from random.org""")
     @command
     def random8(self, args, sender_nick, **kwargs):
         result, error = self.random_impl(args, 8, sender_nick)
         if result: self.bot.say(f'0o{result}')
         else: self.bot.say(error)
 
-    @doc("""random16 <min> <max>: fetch hex random number in [min, max) from random.org
-            random16: fetch hex random number in [0, 1000000000) from random.org""")
+    @doc("""random16 <min> <max>: fetch hex random number in [min, max] from random.org
+            random16: fetch hex random number in [0, 1000000000] from random.org""")
     @command
     def random16(self, args, sender_nick, **kwargs):
         result, error = self.random_impl(args, 16, sender_nick)
