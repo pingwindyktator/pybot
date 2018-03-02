36070d8c17a1810456a258229948202c11448ac1
pingwindyktator <ja2222ja@gmail.com>
2018-03-02 13:08:09 +0100

    spacex_launches refactored

--- plugins/spacex_launches.py
+++ plugins/spacex_launches.py
@@ -49,42 +49,32 @@ class spacex_launches(plugin):
         raw_response = requests.get(latest_api_uri).content.decode('utf-8')
         return json.loads(raw_response)
 
-    def get_next_launch(self):
-        return self.get_upcoming_launches()[0]
-
     def check_upcoming_launches(self):
-        self.logger.info('checking next upcoming launch...')
+        self.logger.info('checking upcoming launches...')
         next_launches = self.get_upcoming_launches()
 
         for next_launch in next_launches:
-            flight_id = next_launch['flight_number']
-            next_launch_time = datetime.fromtimestamp(next_launch['launch_date_unix']) if next_launch['launch_date_unix'] else None
+            self.check_upcoming_launch(next_launch)
 
-            if flight_id in self.upcoming_launches_timers:
-                if not next_launch_time or (self.upcoming_launches_timers[flight_id].launch_datetime != next_launch_time):
-                    self.logger.info(f'launch {flight_id} was postponed, setting new timers')
+    def check_upcoming_launch(self, next_launch):
+        flight_id = next_launch['flight_number']
+        next_launch_time = datetime.fromtimestamp(next_launch['launch_date_unix']) if next_launch['launch_date_unix'] else None
 
-                    for timer in self.upcoming_launches_timers[flight_id].timers:
-                        timer.cancel()
+        if flight_id in self.upcoming_launches_timers:
+            if self.upcoming_launches_timers[flight_id].launch_datetime != next_launch_time:
+                old_launch_time = self.upcoming_launches_timers[flight_id].launch_datetime
+                self.inform_postponed_launch(next_launch, old_launch_time)
+                self.logger.debug(f'canceling timers for {flight_id}')
+                for timer in self.upcoming_launches_timers[flight_id].timers: timer.cancel()
+                del self.upcoming_launches_timers[flight_id]
+            else:
+                self.logger.debug(f'timers for {flight_id} already set')
+                return
 
-                    users_to_call = self.get_users_to_call()
+        self.logger.info(f'setting launch time for {flight_id}: {next_launch_time}')
+        self.upcoming_launches_timers[flight_id] = self.upcoming_launch_info(next_launch_time, [])
 
-                    if self.config['inform_about_postponed_launches'] and users_to_call:
-                        if self.config['call_users_for_postponed_launches']: prefix = ', '.join(users_to_call) + ': '
-                        else: prefix = ''
-                        old_time = color.green(self.upcoming_launches_timers[flight_id].launch_datetime.strftime('%Y-%m-%d %H:%M'))
-                        new_time = color.green(next_launch_time.strftime('%Y-%m-%d %H:%M') if next_launch_time else '<unknown>')
-                        suffix = f'{color.cyan(next_launch["rocket"]["rocket_name"])} launch {color.orange(flight_id)} was just postponed: {old_time} -> {new_time}'
-                        self.bot.say(f'{prefix}{suffix}')
-                        self.bot.say(self.get_launch_info_str(next_launch))
-
-                    if next_launch_time:
-                        self.upcoming_launches_timers[flight_id].timers.clear()
-                    else:
-                        del self.upcoming_launches_timers[flight_id]
-
-            if not next_launch_time: continue
-
+        if next_launch_time:
             self.add_reminder_at(next_launch_time - timedelta(hours=24), flight_id, next_launch_time)
             self.add_reminder_at(next_launch_time - timedelta(hours=1), flight_id, next_launch_time)
             self.add_reminder_at(next_launch_time - timedelta(minutes=20), flight_id, next_launch_time)
@@ -93,14 +83,32 @@ class spacex_launches(plugin):
         now = datetime.now()
         if time < now: return
 
-        if flight_id not in self.upcoming_launches_timers:
-            self.upcoming_launches_timers[flight_id] = self.upcoming_launch_info(launch_time, [])
-
         timer = Timer((time - now).total_seconds(), self.remind_upcoming_launch, kwargs={'flight_id': flight_id})
         self.upcoming_launches_timers[flight_id].timers.append(timer)
         self.upcoming_launches_timers[flight_id].launch_datetime = launch_time
         timer.start()
-        self.logger.info(f'reminder at {time} set for next upcoming launch: {flight_id}')
+        self.logger.info(f'reminder at {time} set for upcoming launch: {flight_id}')
+
+    def inform_postponed_launch(self, launch, old_launch_time):
+        users_to_call = self.get_users_to_call()
+        flight_id = launch['flight_number']
+        new_launch_time = datetime.fromtimestamp(launch['launch_date_unix']) if launch['launch_date_unix'] else None
+        assert new_launch_time != old_launch_time
+
+        old_time_str = color.green(old_launch_time.strftime('%Y-%m-%d %H:%M') + utils.get_str_utc_offset() if old_launch_time else '<unknown>')
+        new_time_str = color.green(new_launch_time.strftime('%Y-%m-%d %H:%M') + utils.get_str_utc_offset() if new_launch_time else '<unknown>')
+        self.logger.info(f'launch {flight_id} was just postponed: {old_time_str} -> {new_time_str}')
+
+        if self.config['inform_about_postponed_launches'] and users_to_call:
+            if self.config['call_users_for_postponed_launches']: prefix = ', '.join(users_to_call) + ': '
+            else: prefix = ''
+
+            rocket_name = color.cyan(launch['rocket']['rocket_name'])
+            flight_id_str = color.orange(flight_id)
+
+            suffix = f'{rocket_name} launch {flight_id_str} was just postponed: {old_time_str} -> {new_time_str}'
+            self.bot.say(f'{prefix}{suffix}')
+            self.bot.say(self.get_launch_info_str(launch))
 
     def remind_upcoming_launch(self, flight_id):
         self.logger.info(f'reminding about next upcoming launch: {flight_id}')
@@ -108,7 +116,13 @@ class spacex_launches(plugin):
         if not to_call: return
 
         launch = self.get_launch_by_id(flight_id)
-        if launch['launch_success'] is not None or not launch['launch_date_unix'] or (datetime.fromtimestamp(launch['launch_date_unix']) < datetime.now()):
+        launch_time = datetime.fromtimestamp(launch['launch_date_unix']) if launch['launch_date_unix'] else None
+
+        if launch['launch_success'] is not None \
+                or not launch['launch_date_unix'] \
+                or (launch_time < datetime.now()) \
+                or flight_id not in self.upcoming_launches_timers \
+                or launch_time != self.upcoming_launches_timers[flight_id].launch_datetime:
             self.logger.warning(f'launch {flight_id} probably canceled / postponed, skipping...')
             return
 
