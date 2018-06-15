5e592fb64ff9ede7f9b622cd4cf672649b271455
pingwindyktator <ja2222ja@gmail.com>
2018-06-15 14:24:38 +0200

    worldcup2018.wc_last, worldcup2018.wc_now

--- plugins/worldcup2018.py
+++ plugins/worldcup2018.py
@@ -5,23 +5,36 @@ from threading import Timer
 from datetime import datetime, timedelta, timezone
 from plugin import *
 
+# TODO features wiki update
+# TODO logging
+# TODO thread safety
+# TODO wc_remind
+# TODO docs
+
 
 class worldcup2018(plugin):
     def __init__(self, bot):
         super().__init__(bot)
         self.match_timers = []
         self.next_matches_info = []
+        self.last_matches_info = []
+        self.in_play_matches_info = []
         self.update_match_timer = utils.repeated_timer(timedelta(minutes=60).total_seconds(), self.update_match_timers)
         self.update_match_timer.start()
 
     class match_desc:
-        def __init__(self, home_team, away_team, date):
+        def __init__(self, home_team, away_team, date, goals_home_team, goals_away_team):
             self.home_team = home_team
             self.away_team = away_team
             self.date = date
+            self.goals_home_team = goals_home_team
+            self.goals_away_team = goals_away_team
 
         def to_response(self):
-            return f'{color.cyan(self.home_team)} - {color.cyan(self.away_team)} starts at {color.green(self.date)}'
+            if self.goals_away_team is None or self.goals_away_team is None:
+                return f'{color.cyan(self.home_team)} - {color.cyan(self.away_team)} starts at {color.green(self.date)}'
+            else:
+                return f'{color.cyan(self.home_team)} {self.goals_home_team} - {self.goals_away_team} {color.cyan(self.away_team)}'
 
     def unload_plugin(self):
         for t in self.match_timers:
@@ -31,37 +44,61 @@ class worldcup2018(plugin):
         return json.loads(requests.get(r'http://api.football-data.org/v1/competitions/467/fixtures').content.decode())
 
     def update_match_timers(self):
-        for t in self.match_timers: t.cancel()
-        self.match_timers.clear()
-        self.next_matches_info.clear()
-
         api_response = self.get_api_response()
         now = datetime.now()
+        match_timers = []
+        next_matches_info = []
+        last_matches_info = []
+        in_play_matches_info = []
 
         for match in api_response['fixtures']:
             home_team = match['homeTeamName']
             away_team = match['awayTeamName']
+            goals_home_team = match['result']['goalsHomeTeam'] if match['result'] and 'goalsHomeTeam' in match['result'] else None
+            goals_away_team = match['result']['goalsAwayTeam'] if match['result'] and 'goalsAwayTeam' in match['result'] else None
 
             assert match['date'][-1] == 'Z'
             match_date = datetime.strptime(match['date'][:-1], "%Y-%m-%dT%H:%M:%S")
             match_date = match_date.replace(tzinfo=timezone.utc).astimezone(tz=None).replace(tzinfo=None)
-            match_desc = self.match_desc(home_team, away_team, match_date)
+            match_desc = self.match_desc(home_team, away_team, match_date, goals_home_team, goals_away_team)
 
             remind_at = match_date - timedelta(minutes=15)
             if self.config['remind_before_match'] and remind_at > now and match['status'] == 'TIMED':
                 self.logger.debug(f'setting match reminder for {home_team} - {away_team}: {remind_at}')
                 t = Timer((remind_at - now).total_seconds(), self.remind_upcoming_match, kwargs={'match_desc': match_desc})
-                self.match_timers.append(t)
+                match_timers.append(t)
                 t.start()
 
             if match['status'] == 'TIMED':
-                self.next_matches_info.append(match_desc)
+                next_matches_info.append(match_desc)
 
-        self.next_matches_info.sort(key=lambda md: md.date)
+            if match['status'] == 'FINISHED':
+                last_matches_info.append(match_desc)
+
+            if match['status'] == 'IN_PLAY':
+                in_play_matches_info.append(match_desc)
+
+        next_matches_info.sort(key=lambda md: md.date)
+        last_matches_info.sort(key=lambda md: md.date, reverse=True)
+        for t in self.match_timers: t.cancel()
+        self.match_timers = match_timers
+        self.next_matches_info = next_matches_info
+        self.last_matches_info = last_matches_info
+        self.in_play_matches_info = in_play_matches_info
 
     @command
     def wc_next(self, **kwargs):
         for md in self.next_matches_info[:3]:
+            self.bot.say(md.to_response())
+
+    @command
+    def wc_last(self, **kwargs):
+        for md in self.last_matches_info[:3]:
+            self.bot.say(md.to_response())
+
+    @command
+    def wc_now(self, **kwargs):
+        for md in self.in_play_matches_info:
             self.bot.say(md.to_response())
 
     def remind_upcoming_match(self, match_desc):
