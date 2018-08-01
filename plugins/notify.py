d65dcf0b79f8ae9e64e6ec06cdb0412e7dbadc2d
pingwindyktator <ja2222ja@gmail.com>
2018-08-01 23:42:35 +0200

    words_blacklist: db support

--- plugins/notify.py
+++ plugins/notify.py
@@ -6,8 +6,7 @@ from plugin import *
 class notify(plugin):
     def __init__(self, bot):
         super().__init__(bot)
-        self.database = {}  # username -> {words to watch}
-        # TODO db
+        self.database = {}  # username -> {words to watch}  # TODO db
 
     def on_pubmsg(self, source, msg, **kwargs):
         if self.bot.is_user_ignored(source.nick): return
