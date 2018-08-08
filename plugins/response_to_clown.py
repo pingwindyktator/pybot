1a187dc1f75d84c0d5bf2556c34b24aa2c840da3
hbdgr <hbdgr@protonmail.com>
2018-08-08 12:37:57 +0200

    update response_to_clown

--- plugins/response_to_clown.py
+++ plugins/response_to_clown.py
@@ -1,3 +1,5 @@
+import re
+
 from plugin import *
 
 
@@ -5,17 +7,25 @@ class response_to_clown(plugin):
     def __init__(self, bot):
         super().__init__(bot)
         self.kick_counter = 3
-        self.clown = 'daniel1302'
-        self.annoying_tab = ['jak życie', 'jakżycie', 'jak zycie', 'jakzycie']
+        self.clown = irc_nickname('daniel1302')
+        self.riemann = irc_nickname('riemann')
+        self.annoying_msg = re.compile('jak.*?ycie', re.IGNORECASE)
+
+    def is_same_nickname(self, a, b):
+        strip_str = ' _' + string.digits
+        a = a.casefold().strip(strip_str)
+        b = b.casefold().strip(strip_str)
+        return a == b and a
 
     def on_pubmsg(self, source, msg, **kwargs):
-        if source.nick == self.clown and 'riemann' in msg:
-            for possible_annoying in self.annoying_tab:
-                if possible_annoying in msg:
-                    if self.kick_counter:
-                        self.bot.say("{}!".format(self.kick_counter))
-                        self.kick_counter -= 1
-                    else:
-                        self.kick_counter = 3
-                        self.bot.kick(source.nick, 'You are so boring..')
+        if self.riemann in msg                          \
+          and is_same_nickname(source.nick, self.clown) \
+          and annoying_msg.match(msg):
+            if self.kick_counter:
+                self.bot.say(f'{self.kick_counter}!')
+                self.kick_counter -= 1
+            else:
+                self.kick_counter = 3
+                self.bot.kick(self.clown, 'You are so boring..')
+                self.logger.warning(f'{self.clown} kicked')
 
