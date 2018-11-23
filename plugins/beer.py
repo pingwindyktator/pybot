2d0bf30913b0226303d734ea15cd44b6154e1371
pingwindyktator <ja2222ja@gmail.com>
2018-11-23 11:43:44 +0100

    beer.beer_rm implemented, beer.beer_reset is more talkative

--- plugins/beer.py
+++ plugins/beer.py
@@ -30,8 +30,26 @@ class beer(plugin):
             return
 
         self.logger.info(f'{sender_nick} gives {nickname} beer')
-        self.update_beers(sender_nick, nickname)
-        self.beer_get_say(sender_nick, nickname)
+        self.add_beer(sender_nick, nickname)
+        self.bot.say(self.get_beers_str(sender_nick, nickname))
+
+    @command
+    @doc('beer_rm <nickname>: take <nickname> a beer')
+    def beer_rm(self, sender_nick, args, **kwargs):
+        if not args: return
+        nickname = irc_nickname(args[0])
+
+        if nickname == sender_nick:
+            self.bot.say('orly?')
+            return
+
+        if nickname == self.bot.get_nickname():
+            self.bot.say(':(')
+            return
+
+        self.logger.info(f'{sender_nick} removes {nickname} beer')
+        self.remove_beer(sender_nick, nickname)
+        self.bot.say(self.get_beers_str(sender_nick, nickname))
 
     @command
     @doc('beer_get <nickname>: get beers owned <nickname>')
@@ -43,7 +61,7 @@ class beer(plugin):
             self.bot.say('orly?')
             return
 
-        self.beer_get_say(sender_nick, nickname)
+        self.bot.say(self.get_beers_str(sender_nick, nickname))
 
     @command
     @doc('beer_reset <nickname>: reset owned beers')
@@ -56,12 +74,13 @@ class beer(plugin):
             self.bot.say('orly?')
             return
 
-        self.logger.info(f'{sender_nick} resets {nickname} beers')
+        self.logger.info(f'{sender_nick} resets {nickname} beers, was {self.get_beers_str(sender_nick, nickname)}')
+        old = self.get_beers_str(sender_nick, nickname)
         self.reset(sender_nick, nickname)
-        self.beer_get_say(sender_nick, nickname)
+        self.bot.say(f'{self.get_beers_str(sender_nick, nickname)}, was {old}')
 
-    def beer_get_say(self, who, to_whom):
-        self.bot.say(f'{who} {self.get_beers(to_whom, who)}:{self.get_beers(who, to_whom)} {to_whom}')
+    def get_beers_str(self, who, to_whom):
+        return f'{who} {self.get_beers(to_whom, who)}:{self.get_beers(who, to_whom)} {to_whom}'
 
     def get_beers(self, who, to_whom):
         with self.db_mutex:
@@ -79,8 +98,14 @@ class beer(plugin):
             with suppress(sqlite3.OperationalError): self.db_cursor.execute(f"INSERT OR REPLACE INTO '{b.casefold()}' VALUES (?, ?)", (a.casefold(), 0))
             self.db_connection.commit()
 
-    def update_beers(self, who, to_whom):
+    def add_beer(self, who, to_whom):
         with self.db_mutex:
             self.db_cursor.execute(f"CREATE TABLE IF NOT EXISTS '{who.casefold()}' (nickname TEXT primary key not null, beers INTEGER)")  # nickname -> beers
             self.db_cursor.execute(f"INSERT OR REPLACE INTO '{who.casefold()}' VALUES (?, COALESCE((SELECT beers + 1 FROM '{who.casefold()}' WHERE nickname = ?), 1))", (to_whom.casefold(), to_whom.casefold()))
+            self.db_connection.commit()
+
+    def remove_beer(self, who, to_whom):
+        with self.db_mutex:
+            self.db_cursor.execute(f"CREATE TABLE IF NOT EXISTS '{who.casefold()}' (nickname TEXT primary key not null, beers INTEGER)")  # nickname -> beers
+            self.db_cursor.execute(f"INSERT OR REPLACE INTO '{who.casefold()}' VALUES (?, COALESCE((SELECT MAX(beers - 1, 0) FROM '{who.casefold()}' WHERE nickname = ?), 0))", (to_whom.casefold(), to_whom.casefold()))
             self.db_connection.commit()
