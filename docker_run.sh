dde9bfb8fa6239bf9d66ae740f0717b6ba2b8ce2
pingwindyktator <ja2222ja@gmail.com>
2019-03-02 15:19:29 +0100

    update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-docker run --user appuser -it --rm -v /etc/localtime:/etc/localtime:ro -v $SCRIPT_DIR:/pybot:z pybot
+docker run --user appuser -it --rm -v ${SCRIPT_DIR}:/pybot:z pybot
